9df1f5421148a76342a850af9ba0c4acf8215a93
ambaldwin21 <anna.baldwin21@gmail.com>
2016-10-21 15:17:31 -0600

    heroku address instead of local

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(passport.initialize());
 passport.use(new FacebookStrategy({
   clientID: '1296493123736207',
   clientSecret: '54a5c28e3b0fd0cd37d19c04c6c542a6',
-  callbackURL: "http://localhost:3000/auth/facebook/callback",
+  callbackURL: "https://friends-blog.herokuapp.com/",
   profileFields: ['id', 'name', 'profileUrl', 'email'],
   enableProof: true,
   passReqToCallback: true
